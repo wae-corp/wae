a592fdc2718d8f21639ee2f51e8fd2846e6b1057
Rahul Patil <rahulpwos@gmail.com>
2024-11-22 15:55:58 +0530

    Sustainability Case Studies page responsive done

--- app/routes/sustainability-case-study.tsx
+++ app/routes/sustainability-case-study.tsx
@@ -10,33 +10,33 @@ export const meta: MetaFunction = () => {
 export default function SustainabilityCaseStudy() {
   return (
     <>
-      <main className="relative flex min-h-screen items-center bg-water-splash bg-cover bg-top bg-no-repeat pt-[var(--header-height)] text-white">
-        <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent"></div>
+      <main className="relative flex h-screen items-center bg-water-splash bg-cover bg-top bg-no-repeat pt-[var(--header-height)] text-white md:h-[640px] md:items-end md:pb-[120px] xl:items-center">
+        <div className="absolute inset-0 bg-black/70"></div>
         <div
           className="container isolate text-center"
           data-aos="fade-down"
         >
-          <h1 className="wae-h2 font-base mb-10 font-secondary uppercase xl:text-[70px]">
+          <h1 className="font-base mb-10 font-secondary text-[40px] uppercase leading-relaxed md:mb-5 md:text-[54px] md:leading-normal xl:mb-[32px] xl:text-[70px]">
             WAE's Solutions Driving Change!
           </h1>
 
-          <p className="wae-h6 mb-10 uppercase 2xl:text-3xl">
+          <p className="text-sm uppercase leading-relaxed md:text-base xl:text-xl">
             How the BFSI Sector is Leading the Green Revolution
           </p>
         </div>
       </main>
 
-      <section>
+      <section className="py-20 md:pb-[120px] md:pt-[60px] xl:pt-[120px]">
         <div className="container">
-          <div className="wae-pb-lg wae-pt-lg container flex items-center 2xl:min-h-screen">
-            <div className="flex flex-col gap-8 lg:flex-row lg:gap-[140px]">
+          <div className="flex items-center">
+            <div className="flex flex-col gap-10 md:gap-[60px] xl:flex-row xl:gap-[140px]">
               <div
               // data-aos="fade-right"
               >
-                <h3 className="wae-h4 mb-10 font-secondary">
+                <h3 className="mb-5 font-secondary text-2xl xl:mb-12 xl:text-4xl">
                   Transforming Hydration in the BFSI Sector
                 </h3>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 text-xs md:text-sm xl:text-base">
                   <div className="prefix-dot"></div>
                   <p className="uppercase">
                     Explore how BFSI leaders are not just crunching numbers but
@@ -49,7 +49,7 @@ export default function SustainabilityCaseStudy() {
                 // data-aos="fade-left"
                 className="flex-shrink-0 lg:max-w-[400px]"
               >
-                <p className="wae-h6 uppercase">
+                <p className="text-sm uppercase md:text-base xl:text-xl">
                   In an era where every drop counts, the BFSI (Banking,
                   Financial Services, and Insurance) sector is taking bold steps
                   toward sustainability. Through partnerships with WAE, leading
@@ -67,10 +67,10 @@ export default function SustainabilityCaseStudy() {
         </div>
       </section>
 
-      <section className="relative flex min-h-screen items-center bg-leaf-closeup bg-cover bg-top bg-no-repeat text-white">
+      <section className="relative flex h-screen items-center bg-leaf-closeup bg-cover bg-top bg-no-repeat text-white">
         <div className="absolute inset-0 z-0 bg-gradient-to-b from-black/70 via-black to-black/70"></div>
         <div className="container isolate">
-          <h3 className="wae-h3 text-center font-black uppercase">
+          <h3 className="text-center text-2xl font-black uppercase leading-relaxed md:text-[32px] md:leading-relaxed xl:text-[42px]">
             Spotlight on BFSI Sustainability Champions
           </h3>
         </div>
@@ -80,22 +80,24 @@ export default function SustainabilityCaseStudy() {
         {championsSpotlight.map((champion, idx) => (
           <section
             className={
-              "wae-pt-lg wae-pb-lg group text-white odd:bg-black odd:text-white even:bg-white even:text-black"
+              "group py-20 text-white odd:bg-black odd:text-white even:bg-white even:text-black md:py-[120px] xl:py-[180px]"
             }
             key={idx}
           >
             <div className="container">
-              <div className="wae-mb-lg flex flex-col items-start gap-8 lg:flex-row lg:gap-14">
+              <div className="mb-20 flex flex-col items-start gap-10 md:mb-[60px] lg:flex-row lg:gap-14 xl:mb-[180px]">
                 <div
                 // data-aos="fade-right"
                 >
-                  <h3 className="wae-h3 mb-8 font-secondary uppercase !leading-normal lg:mb-10 lg:text-[44px]">
+                  <h3 className="mb-5 font-secondary text-2xl uppercase !leading-normal md:mb-4 md:text-[32px] xl:mb-10 xl:text-[44px]">
                     {champion.title}
                   </h3>
 
                   <div className="flex items-center gap-4">
                     <div className="prefix-dot mr-0"></div>
-                    <p className="text-base uppercase">{champion.subtitle}</p>
+                    <p className="text-xs uppercase md:text-base">
+                      {champion.subtitle}
+                    </p>
                   </div>
                 </div>
                 <img
@@ -103,16 +105,15 @@ export default function SustainabilityCaseStudy() {
                   alt={champion.title}
                   width={600}
                   height={384}
-                  className="block shrink-0 object-cover max-xl:mx-auto"
+                  className="block shrink-0 object-cover max-xl:mx-auto xl:w-[600px]"
                   data-aos="fade-zoom-in"
                   data-aos-easing="ease-in-back"
-                  data-aos-offset="200"
                   data-aos-duration="500"
                 />
               </div>
 
               <div
-                className={clsx("mb-8 grid gap-y-12 lg:mb-[60px]", {
+                className={clsx("mb-10 grid gap-y-[60px] xl:mb-[60px]", {
                   "mx-auto max-w-[900px] sm:grid-cols-2":
                     champion.achievments?.length === 2,
                   "sm:grid-cols-3": champion.achievments?.length === 3,
@@ -125,7 +126,7 @@ export default function SustainabilityCaseStudy() {
                     data-aos="fade-up"
                     data-aos-delay={`${idx}00`}
                   >
-                    <h2 className="mb-5 font-secondary text-4xl uppercase lg:text-[50px]">
+                    <h2 className="mb-5 font-secondary text-4xl font-semibold uppercase md:mb-3 md:text-[32px] xl:mb-5 xl:text-[50px] xl:font-medium">
                       <Counter
                         end={achievment.title}
                         duration={1}
@@ -133,27 +134,29 @@ export default function SustainabilityCaseStudy() {
                       />{" "}
                       {achievment.unit}
                     </h2>
-                    <p className="uppercase">{achievment.subtitle}</p>
+                    <p className="uppercase md:text-sm xl:text-base">
+                      {achievment.subtitle}
+                    </p>
                   </div>
                 ))}
               </div>
 
               <p
-                className="text-center text-base uppercase"
+                className="text-center text-base uppercase md:text-sm"
                 data-aos="fade-in"
               >
                 {champion.statement}
               </p>
 
               <div
-                className="flex justify-center"
+                className="mt-10 flex justify-center md:mt-[60px] lg:mt-20 xl:mt-20"
                 // data-aos="fade-in"
                 data-aos-delay="100"
               >
                 <Link
                   to={champion.link}
                   className={
-                    "wae-btn mt-10 px-4 group-odd:border-white group-even:border-black lg:mt-20"
+                    "wae-btn px-4 py-3 group-odd:border-white group-even:border-black max-md:w-full"
                   }
                 >
                   DOWNLOAD SUCCESS STORIES
@@ -166,14 +169,16 @@ export default function SustainabilityCaseStudy() {
 
       <section>
         <div className="container">
-          <div className="mb-10 flex items-center justify-center py-20 text-center text-black lg:py-40">
+          <div className="mb-20 flex items-center justify-center text-center text-black md:mb-[60px] xl:py-40 xl:pb-0">
             <div
               className="mx-auto"
               // data-aos="fade-down"
             >
-              <h6 className="uppercase">Why WAE's Green Machines Matter</h6>
+              <h6 className="mb-10 text-xs uppercase md:mb-[60px] md:text-sm">
+                Why WAE's Green Machines Matter
+              </h6>
 
-              <h5 className="wae-h5-lg mb-12 mt-12 font-secondary leading-tight lg:mb-[100px]">
+              <h5 className="font-secondary text-2xl uppercase leading-tight lg:mb-[100px]">
                 WAE's hydration solutions are engineered to perfection, ensuring
                 minimal water wastage and eliminating plastic waste. Our
                 advanced ROVPD technology recovers 70% of water during the
@@ -185,72 +190,71 @@ export default function SustainabilityCaseStudy() {
             </div>
           </div>
 
-          <h5
-            // data-aos="fade-right"
-            className="wae-h5-lg mb-8 !leading-normal lg:mb-10"
-          >
-            The Value Proposition:
-          </h5>
-          <div className="wae-mb-lg wae-gap-lg flex flex-col items-start lg:flex-row">
-            <div
-            // data-aos="fade-right"
+          <div className="pb-10 md:pb-[120px]">
+            <h5
+              // data-aos="fade-right"
+              className="mb-10 text-2xl !leading-normal xl:mb-[60px] xl:text-[32px]"
             >
-              <div className="mb-8 flex items-start gap-4 lg:mb-[60px]">
-                <div className="prefix-dot mr-0 mt-2"></div>
-                <div>
-                  <h6 className="wae-h6-lg mb-5 font-secondary uppercase">
-                    Cost Efficiency:
-                  </h6>
-                  <p className="text-base uppercase">
-                    With WAE drinking water stations, organizations can cut
-                    water bills by 70%, save precious office space, and
-                    eliminate the need for bottled water logistics.
-                  </p>
+              The Value Proposition:
+            </h5>
+            <div className="flex flex-col items-start md:flex-row md:gap-4 xl:gap-[150px]">
+              <div className="mb-10 md:mb-0">
+                <div className="mb-10 flex items-start gap-4 lg:mb-[60px]">
+                  <div className="prefix-dot mr-0 mt-2"></div>
+                  <div>
+                    <h6 className="mb-3 font-secondary uppercase md:mb-5 md:text-xl xl:text-2xl">
+                      Cost Efficiency:
+                    </h6>
+                    <p className="text-xs uppercase md:text-sm xl:text-base">
+                      With WAE drinking water stations, organizations can cut
+                      water bills by 70%, save precious office space, and
+                      eliminate the need for bottled water logistics.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-start gap-4">
+                  <div className="prefix-dot mr-0 mt-2"></div>
+                  <div>
+                    <h6 className="mb-3 font-secondary uppercase md:mb-5 md:text-xl xl:text-2xl">
+                      Health Benefits:
+                    </h6>
+                    <p className="text-xs uppercase md:text-sm xl:text-base">
+                      Unlike bottled water, which can leach harmful
+                      microplastics and chemicals like BPA, WAE's purified water
+                      is free from toxins, offering safe hydration without the
+                      environmental downsides.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-start gap-4">
-                <div className="prefix-dot mr-0 mt-2"></div>
-                <div>
-                  <h6 className="wae-h6-lg mb-5 font-secondary uppercase">
-                    Health Benefits:
-                  </h6>
-                  <p className="text-base uppercase">
-                    Unlike bottled water, which can leach harmful microplastics
-                    and chemicals like BPA, WAE's purified water is free from
-                    toxins, offering safe hydration without the environmental
-                    downsides.
-                  </p>
-                </div>
-              </div>
+              <img
+                src="https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/cc74550e-f3cc-43d5-f17c-c308b81ab800/public"
+                alt="Beaker on table"
+                width={500}
+                height={384}
+                className="block shrink-0 object-cover max-xl:mx-auto md:w-[300px] xl:w-[500px]"
+                data-aos="fade-zoom-in"
+                data-aos-easing="ease-in-back"
+                data-aos-duration="500"
+              />
             </div>
-            <img
-              src="https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/cc74550e-f3cc-43d5-f17c-c308b81ab800/public"
-              alt="Beaker on table"
-              width={500}
-              height={384}
-              className="block shrink-0 object-cover max-xl:mx-auto"
-              data-aos="fade-zoom-in"
-              data-aos-easing="ease-in-back"
-              data-aos-offset="200"
-              data-aos-duration="500"
-            />
           </div>
         </div>
       </section>
 
-      <section className="relative flex items-center justify-center bg-black bg-earth-in-hands bg-cover bg-no-repeat pt-20 text-center lg:min-h-screen">
+      <section className="relative flex h-screen items-end justify-center bg-black bg-earth-in-hands bg-cover bg-no-repeat py-[30px] text-center md:h-[520px] md:pb-[60px] xl:h-[720px] xl:pb-[100px]">
         <div className="absolute inset-0 h-full w-full bg-black/50"></div>
         <div className="container isolate">
           <div
             className="mx-auto text-white lg:mt-40"
-            data-aos="fade-uo"
+            data-aos="fade-up"
           >
-            <p className="wae-h6-lg font-light uppercase">
-              <span className="prefix-dot mr-5"></span>
-              Join the Movement Towards a Greener Future
-            </p>
+            <div className="mb-10 flex justify-center font-light uppercase xl:text-2xl">
+              <span className="prefix-dot mr-5 mt-1"></span>
+              <p>Join the Movement Towards a Greener Future</p>
+            </div>
 
-            <h5 className="wae-h5-lg mt-10 font-secondary uppercase leading-tight">
+            <h5 className="font-secondary text-xl uppercase !leading-relaxed md:text-2xl xl:text-[32px]">
               The BFSI sector is showing that sustainable choices have a ripple
               effect. By embracing WAE's innovative hydration solutions, these
               organizations are not just quenching thirst they're feeding a
@@ -260,26 +264,26 @@ export default function SustainabilityCaseStudy() {
         </div>
       </section>
 
-      <section className="relative flex items-center border-b border-white bg-black bg-cover bg-top bg-no-repeat py-20 text-center text-white lg:min-h-screen">
+      <section className="relative flex items-center border-b border-white bg-black bg-cover bg-top bg-no-repeat py-20 text-center text-white md:py-[120px] lg:min-h-screen">
         <div className="container">
           <div
             className="mx-auto max-w-[860px]"
             data-aos="fade-up"
           >
-            <h3 className="wae-h3-lg mb-6 text-center font-secondary uppercase lg:mb-10 lg:text-[50px]">
+            <h3 className="mb-5 text-center font-secondary text-2xl uppercase lg:mb-10 lg:text-[50px]">
               Ready to make a change?
             </h3>
 
-            <p className="text-lg uppercase xl:text-2xl">
+            <p className="text-base uppercase xl:text-2xl">
               Partner with WAE and start making every drop count for your
               business and the planet.
             </p>
 
             <Link
               to={"/"}
-              className="wae-btn wae-btn-md mt-10 border-white px-6 lg:mt-[100px]"
+              className="wae-btn wae-btn-md mt-10 gap-3 border-white px-6 py-3 max-md:w-full xl:mt-[100px]"
             >
-              Contact Us {Icons.ChevronRight}
+              Contact Us {Icons.ArrowRightLong}
             </Link>
           </div>
         </div>
