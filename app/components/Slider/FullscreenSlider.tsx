71e1a662c790731702b37c30a1cb3e2f5e4ae590
Harshit004 <69170819+Harshit004@users.noreply.github.com>
2025-02-26 11:16:56 +0530

    made sure the button text does not have a line break.

--- app/components/Slider/FullscreenSlider.tsx
+++ app/components/Slider/FullscreenSlider.tsx
@@ -155,8 +155,8 @@ export const FullscreenSlider = ({items}: SliderProps) => {
                     <Link
                       to={idx === 1 ? "/products" : idx === 2 ? "/sustainability" : "/contact"} // Dynamically change link
                       className="wae-btn-light wae-btn wae-btn-lg !border-none px-10 py-3 text-[16px] 
-                      w-[200px] h-[48px] font-poppins md:text-[16px] 
-                      sm:w-[160px] sm:h-[40px] sm:text-[14px] md:px-6"
+                      w-auto min-w-[200px] h-[48px] font-poppins md:text-[16px] 
+                      sm:w-[160px] sm:h-[40px] sm:text-[14px] md:px-6 whitespace-nowrap"
                     >
                       {item.buttonText}
                     </Link>
